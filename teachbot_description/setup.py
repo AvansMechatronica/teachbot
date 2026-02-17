788bc4dc4013f370bda87bc9afdec454c40bbb46
Gerard Harkema <GerardAnneHarkema@gmail.com>
2026-02-17 12:07:42 +0100

    added teachbot_description

---
+++ teachbot_description/setup.py
@@ -0,0 +1,58 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'teachbot_description'
+
+data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ]
+
+
+def package_files(data_files, directory_list):
+
+    paths_dict = {}
+
+    for directory in directory_list:
+
+        for (path, directories, filenames) in os.walk(directory):
+
+            for filename in filenames:
+
+                file_path = os.path.join(path, filename)
+                install_path = os.path.join('share', package_name, path)
+
+                if install_path in paths_dict.keys():
+                    paths_dict[install_path].append(file_path)
+
+                else:
+                    paths_dict[install_path] = [file_path]
+
+    for key in paths_dict.keys():
+        data_files.append((key, paths_dict[key]))
+
+    return data_files
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=package_files(data_files, ['teachbot_description', 'launch/', 'urdf/', 'meshes/', 'rviz/']),
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='gerard',
+    maintainer_email='GerardAnneHarkema@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
